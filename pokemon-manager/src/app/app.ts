504dcee24a3c689043ef64cc54edceef279f3f9b
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-01-10 16:06:30 +0100

    Feat: update app structure with router outlet and nav-bar styles

--- pokemon-manager/src/app/app.ts
+++ pokemon-manager/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { NavBar } from './components/nav-bar/nav-bar';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBar],
+  standalone: true,
+  imports: [NavBar, RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('pokemon-manager');
+  title = 'pokemon-manager';
 }
