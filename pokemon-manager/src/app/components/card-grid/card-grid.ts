737b545455e41247b00bf7cf1e3142f26f971cfb
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-01-25 19:07:37 +0100

    Feat: implement card grid component with dynamic rendering of cards

--- pokemon-manager/src/app/components/card-grid/card-grid.ts
+++ pokemon-manager/src/app/components/card-grid/card-grid.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Root } from '../interfaces/i-pokemon';
 
 @Component({
   selector: 'app-card-grid',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './card-grid.html',
   styleUrl: './card-grid.css',
 })
-export class CardGrid {
-
+export class CardGridComponent {
+  @Input() cards: Root[] = [];
 }
