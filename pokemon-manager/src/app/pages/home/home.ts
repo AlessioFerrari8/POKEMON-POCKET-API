98e95aaf2910ea7bcb8a8cad33e23e12cb7ce80d
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-01-29 18:27:19 +0100

    Feat: update home page layout and integrate ScrollAnimate directive for enhanced animations

--- pokemon-manager/src/app/pages/home/home.ts
+++ pokemon-manager/src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScrollAnimateDirective } from '../../directives/scroll-animate.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollAnimateDirective],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
