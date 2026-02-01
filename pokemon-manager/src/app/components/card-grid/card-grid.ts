6fc73f7dbc6252332858655f2705aa1634f8b713
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-02-01 16:17:46 +0100

    Feat: update card-grid for permitting click and card details visualization

--- pokemon-manager/src/app/components/card-grid/card-grid.ts
+++ pokemon-manager/src/app/components/card-grid/card-grid.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IPokemon } from '../interfaces/i-pokemon';
 
@@ -11,4 +11,9 @@ import { IPokemon } from '../interfaces/i-pokemon';
 })
 export class CardGridComponent {
   @Input() cards: IPokemon[] = [];
+  @Output() cardClicked = new EventEmitter<IPokemon>();
+
+  onCardClick(card: IPokemon): void {
+    this.cardClicked.emit(card);
+  }
 }
