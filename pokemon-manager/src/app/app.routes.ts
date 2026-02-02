6442b78a4f6b9da1281d1119e90cbed8fd974ceb
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-02-02 16:14:28 +0100

    Feat: stop routing without login

--- pokemon-manager/src/app/app.routes.ts
+++ pokemon-manager/src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { Cards } from './pages/cards/cards';
 import { Decks } from './pages/decks/decks';
 import { MissingCards } from './pages/missing-cards/missing-cards';
 import { ExpansionDetail } from './pages/expansion-detail/expansion-detail';
+import { Login } from './components/login/login';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
+  { path: 'login', component: Login },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
-  { path: 'cards', component: Cards },
-  { path: 'decks', component: Decks },
-  { path: 'missing-cards', component: MissingCards },
-  { path: 'expansion/:id', component: ExpansionDetail },
+  { path: 'home', component: Home, canActivate: [authGuard] },
+  { path: 'cards', component: Cards, canActivate: [authGuard] },
+  { path: 'decks', component: Decks, canActivate: [authGuard] },
+  { path: 'missing-cards', component: MissingCards, canActivate: [authGuard] },
+  { path: 'expansion/:id', component: ExpansionDetail, canActivate: [authGuard] },
   { path: '**', redirectTo: 'home' }
 ];
