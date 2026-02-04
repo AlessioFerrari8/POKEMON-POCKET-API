243031850e4bdec80f24ae190c51b0de8705e00d
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2026-02-04 10:30:36 +0000

    Fix: env passage of API

--- pokemon-manager/src/app/app.config.ts
+++ pokemon-manager/src/app/app.config.ts
@@ -1,17 +1,33 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
 
+const loadConfig = (http: HttpClient) => () => {
+  return http.get<any>(environment.configUrl).toPromise().then(config => {
+    if (config) {
+      environment.firebase = config.firebase;
+    }
+  }).catch(err => {
+    console.error('Errore nel caricamento di config.json:', err);
+  });
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadConfig,
+      deps: [HttpClient],
+      multi: true
+    },
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth())
   ]
