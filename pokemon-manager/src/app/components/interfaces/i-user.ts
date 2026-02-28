eeec3174dd1a5753a08842852504dd8174d16861
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-02-28 17:42:08 +0100

    Feat: add additional fields to IPokemon interface

--- pokemon-manager/src/app/components/interfaces/i-user.ts
+++ pokemon-manager/src/app/components/interfaces/i-user.ts
@@ -1,7 +1,11 @@
+import { IPokemon } from "./i-pokemon";
+
 export interface IUser {
   uid: string;
   email: string | null;
   displayName: string | null;
   photoURL: string | null;
   emailVerified: boolean;
+  cardsOwnedCount?: number; // non necessariamente cè
+  missingCards?: IPokemon[];
 }
