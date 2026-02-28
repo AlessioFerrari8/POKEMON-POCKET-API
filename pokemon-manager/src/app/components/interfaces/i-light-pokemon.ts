b1193fb3f644c71f6432c3b34b9c453ba50a548f
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-02-28 10:00:11 +0100

    Feat: add light interface pokemon for managing cards on db

---
+++ pokemon-manager/src/app/components/interfaces/i-light-pokemon.ts
@@ -0,0 +1,14 @@
+export interface IPokemon {
+  category: string
+  id: string
+  illustrator: string
+  image: string
+  localId: string
+  name: string
+  rarity: string
+  dexId: number[]
+  hp: number
+  types: string[]
+  stage: string
+}
+
