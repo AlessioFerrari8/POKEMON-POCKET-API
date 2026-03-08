92a20edb43fa65293507efd4a5c3ce1024618f72
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-03-08 16:21:09 +0100

    Fix: hover on stats in user-profile

--- pokemon-manager/src/app/components/user-profile/user-profile.ts
+++ pokemon-manager/src/app/components/user-profile/user-profile.ts
@@ -1,5 +1,6 @@
 import { Component, inject, computed, Signal } from '@angular/core';
 import { UsersService } from '../../services/users-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,10 +10,13 @@ import { UsersService } from '../../services/users-service';
 })
 export class UserProfile {
   private usersService = inject(UsersService);
+  private router = inject(Router);
 
+  // computed means read-only signals
   displayName: Signal<string | null | undefined> = computed(() => this.usersService.userData()?.displayName);
   photoURL: Signal<string | null | undefined> = computed(() => this.usersService.userData()?.photoURL);
   email: Signal<string | null | undefined> = computed(() => this.usersService.userData()?.email);
+  id: Signal<string | null | undefined> = computed(() => this.usersService.userData()?.uid);
 
   cardsOwned: Signal<number> = computed(() => this.usersService.userData()?.cardsOwnedCount ?? 0);
   decksCount: Signal<number> = computed(() => this.usersService.userData()?.decks?.length ?? 0);
@@ -20,6 +24,14 @@ export class UserProfile {
 
   onImageError(): void {
     console.warn('Failed to load profile image');
+  }
+
+  toDecks(): void {
+    this.router.navigateByUrl('/decks');
+  }
+
+  toMissing(): void {
+    this.router.navigateByUrl('/missing-cards');
   }
 }
 
